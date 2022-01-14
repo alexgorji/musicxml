22d00013279e61f4d4c5e78b0122be721a609026
alexgorji <aligorji@hotmail.com>
2022-01-14 20:26:03 +0100

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="musicxml",
+    version="0.1",
+    author="Alex Gorji",
+    author_email="aligorji@hotmail.com",
+    description="generating musicxml",
+    url="https://github.com/alexgorji/musicxml.git",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
