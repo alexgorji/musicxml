3695acca1c77b17b73ec129d236b07ee221a039f
alexgorji <aligorji@hotmail.com>
2022-01-18 17:15:57 +0100

    xsd element score pasted as string in xmlelement and xsdcomplexformat

--- musicxml/generate_classes/defaults/xmlelement.py
+++ musicxml/generate_classes/defaults/xmlelement.py
@@ -8,7 +8,32 @@ from musicxml.xsd.xsdtree import XSDTree
 
 # xml score partwise
 xsd_tree_score_partwise_part = XSDTree(musicxml_xsd_et_root.find(f".//{ns}element[@name='score-partwise']"))
-
+"""
+<xs:element name="score-partwise" block="extension substitution" final="#all">
+    <xs:annotation>
+        <xs:documentation>The score-partwise element is the root element for a partwise MusicXML score. It includes a score-header group followed by a series of parts with measures inside. The document-attributes attribute group includes the version attribute.</xs:documentation>
+    </xs:annotation>
+    <xs:complexType>
+        <xs:sequence>
+            <xs:group ref="score-header"/>
+            <xs:element name="part" maxOccurs="unbounded">
+                <xs:complexType>
+                    <xs:sequence>
+                        <xs:element name="measure" maxOccurs="unbounded">
+                            <xs:complexType>
+                                <xs:group ref="music-data"/>
+                                <xs:attributeGroup ref="measure-attributes"/>
+                            </xs:complexType>
+                        </xs:element>
+                    </xs:sequence>
+                    <xs:attributeGroup ref="part-attributes"/>
+                </xs:complexType>
+            </xs:element>
+        </xs:sequence>
+        <xs:attributeGroup ref="document-attributes"/>
+    </xs:complexType>
+</xs:element>
+"""
 
 class XMLScorePartwise(XMLElement):
     TYPE = XSDComplexTypeScorePartwise
--- musicxml/generate_classes/defaults/xsdcomplextype.py
+++ musicxml/generate_classes/defaults/xsdcomplextype.py
@@ -61,7 +61,32 @@ class XSDComplexType(XSDTreeElement):
 
 
 xsd_tree_score_partwise = XSDTree(musicxml_xsd_et_root.find(".//{*}element[@name='score-partwise']"))
-
+"""
+<xs:element name="score-partwise" block="extension substitution" final="#all">
+    <xs:annotation>
+        <xs:documentation>The score-partwise element is the root element for a partwise MusicXML score. It includes a score-header group followed by a series of parts with measures inside. The document-attributes attribute group includes the version attribute.</xs:documentation>
+    </xs:annotation>
+    <xs:complexType>
+        <xs:sequence>
+            <xs:group ref="score-header"/>
+            <xs:element name="part" maxOccurs="unbounded">
+                <xs:complexType>
+                    <xs:sequence>
+                        <xs:element name="measure" maxOccurs="unbounded">
+                            <xs:complexType>
+                                <xs:group ref="music-data"/>
+                                <xs:attributeGroup ref="measure-attributes"/>
+                            </xs:complexType>
+                        </xs:element>
+                    </xs:sequence>
+                    <xs:attributeGroup ref="part-attributes"/>
+                </xs:complexType>
+            </xs:element>
+        </xs:sequence>
+        <xs:attributeGroup ref="document-attributes"/>
+    </xs:complexType>
+</xs:element>
+"""
 
 class XSDComplexTypeScorePartwise(XSDComplexType):
     XSD_TREE = XSDTree(musicxml_xsd_et_root.findall(".//{*}element[@name='score-partwise']//{*}complexType")[0])
